b3e075f9b152d4d263831551ef67706747a1d56d
chauhanshiv1711-dot <chauhanshiv1711@gmail.com>
2026-06-12 21:42:43 +0530

    backend setup -part2

---
+++ src/db/index.js
@@ -0,0 +1,35 @@
+ import mongoose from "mongoose";
+import { DB_NAME } from "./constant.js";
+
+const connectDB = async () => {
+try {  
+        const connectionInstance = await mongoose.connect( `${process.env.MONGODB_URI}/${DB_NAME}` );
+        console.log("\n MongoDB connected!!! DB HOST:${connectionInstance.connection.host}") ;
+} 
+
+ catch(error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
+};
+export default connectDB;
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constant.js";
+
+// const connectDB = async () => {
+//   try {
+//     const connectionInstance = await mongoose.connect(
+//       `${process.env.MONGODB_URI}/${DB_NAME}`
+//     );
+
+//     console.log(
+//       `\n MongoDB connected!!! DB HOST: ${connectionInstance.connection.host}`
+//     );
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error);
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,48 @@
+//require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config({
+    path: "./.env"
+});
+import connectDB from "./db/index.js";
+import mongoose from "mongoose";
+import{ DB_NAME } from "./constant.js";
+import express from "express";
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+connectDB()
+
+
+
+/*
+(async () => {
+  try {
+    await mongoose.connect( '${process.env.MONGODB_URI}/${DB_NAME}' );
+    app.on("error", (error) => {
+      console.error("Error connecting to MongoDB:", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+        console.log(Server is running on port ${process.env.PORT});
+    });
+  } catch(error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  }
+})();//connectDB()*/
