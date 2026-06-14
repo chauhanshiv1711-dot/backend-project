646a526dea3c1f0d5ee1645f94971c3833356c86
chauhanshiv1711-dot <chauhanshiv1711@gmail.com>
2026-06-14 08:00:10 +0530

    util code

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 import express from "express";
-
+import cors from "cors";
+import cookieParser from "cookie-parser";
 const app = express();
-
-// Middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Health check route
-app.get("/", (req, res) => {
-  res.json({ message: "Server is running!" });
-});
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+}));
+app.use(express.json({limit: '16kb'}));
+app.use(express.urlencoded(({extended: true, limit: '16kb'})));
+app.use(express.static("public"))
+app.use(cookieParser())
+;
 
 export default app;
