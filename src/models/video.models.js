33c10f49e5bc128af94fba5dd766c737151242b4
chauhanshiv1711-dot <chauhanshiv1711@gmail.com>
2026-06-18 18:26:54 +0530

    feat: add models for comments, likes, playlists, and tweets; update user routes for cover image handling

--- src/models/video.models.js
+++ src/models/video.models.js
@@ -37,4 +37,5 @@ const videoSchema = new Schema({
     }
 });
 videoSchema.plugin(mongooseAggregatePaginate);
-export const Video = mongoose.model("Video", videoSchema);
+export
+ const Video = mongoose.model("Video", videoSchema);
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser,refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, getUserChannelProfile, getWatchHistory} from "../controllers/user.controllers.js";
+import { loginUser, logoutUser, registerUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUsercoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 const router = Router();
@@ -18,12 +18,12 @@ router.route("/login").post(loginUser)
 
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
-router.route("/refresh -token").post(refreshAccessToken)
+router.route("/refresh-token").post(refreshAccessToken)
 router.route("/change-password").post(verifyJWT,changeCurrentPassword)
 router.route("/current-user").get(verifyJWT,getCurrentUser)
-router.route("update-account").patch(verifyJWT,updateAccountDetails)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
 router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
-router.route("/coverImage").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+router.route("/coverImage").patch(verifyJWT,upload.single("coverImage"),updateUsercoverImage)
 router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
 router.route("/history").get(verifyJWT,getWatchHistory)
 export default router
